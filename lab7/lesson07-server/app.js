599ef1c7f4b0cf217814cef0111f3f389d8605b9
khelifanasir <100175510+khelifanasir@users.noreply.github.com>
2022-08-02 17:15:45 -0500

    lab7

---
+++ lab7/lesson07-server/app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const cors = require('cors');
+
+const booksroute = require('./routes/booksroutes');
+
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.use('/books', booksroute);
+app.use((req, res, next) => {
+    res.status(404).send({error: "API NOT SUPPORTED"});
+});
+app.use((err, req, res, next) =>{
+    res.status(500).send({error: err.message})
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/library1')
+ .then (()=>{
+    app.listen(3000, ()=> {console.log("listening to 3000")});
+ })
+
